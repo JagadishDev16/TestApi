bbd96de9b79baec3bf96e4b01832c10d1cab0e52
JagadishDev16 <jagadish.dev16@gmail.com>
2025-07-24 00:29:55 -0500

    Update GetTime.cs

--- GetTime.cs
+++ GetTime.cs
@@ -1,1 +1,14 @@
-return DateTime.Now
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/datetime", () =>
+{
+    return DateTime.Now.ToString("o"); // ISO 8601 format
+});
+
+app.Run();
